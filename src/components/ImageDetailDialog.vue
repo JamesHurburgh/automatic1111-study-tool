ef840c04830f7a5a101dc65fb1f5865aa7c48dcd
James Hurburgh <JamesHurburgh@gmail.com>
2023-05-13 20:30:52 +1000

    fix: Cleaned up Image Detail Dialogue

--- src/components/ImageDetailDialog.vue
+++ src/components/ImageDetailDialog.vue
@@ -1,28 +1,34 @@
 <template>
-  <v-dialog fullscreen v-model="showImageDetailDialog">
+  <v-dialog
+    fullscreen
+    v-model="showImageDetailDialog">
     <v-card>
       <v-card-text class="ma-0 pa-0">
-        <v-container fluid class="primary fill-height pa-0">
-          <v-row fluid class="fill-height ma-0 pa-0" no-gutters>
-            <v-col cols="8" no-gutters>
-              <!-- <v-row class="fill-height" justify="center" align="center" :style="`background-image: url(${image.id}); background-size: cover;`">
-
-        </v-row> -->
-              <v-img :src="image.id" :alt="image.metaData?.prompt"
-              :height="$vuetify.display.height"></v-img>
+        <v-container fluid class="pa-0">
+          <v-row
+            class="ma-0 pa-0"
+            no-gutters>
+            <v-col cols="8">
+              <v-img
+                :src="image.id"
+                :alt="image.metaData?.prompt"
+                :height="$vuetify.display.height"
+              ></v-img>
             </v-col>
             <v-col cols="4">
-              <v-toolbar dense dark color="primary">
-                <v-toolbar-title>Images</v-toolbar-title>
+              <v-toolbar
+                color="primary">
+                <v-toolbar-title>Image</v-toolbar-title>
                 <v-spacer></v-spacer>
-
-                <v-btn icon @click.stop="close()">
+                <v-btn
+                  icon
+                  @click.stop="close()">
                   <v-icon>mdi-close</v-icon>
                 </v-btn>
               </v-toolbar>
               <v-textarea readonly density="compact" label="Prompt" hide-details="auto"
-                :model-value="image.metaData?.prompt" append-outer-icon="mdi-content-copy"
-                @click:append-outer="copy(image.metaData?.prompt)"></v-textarea>
+                :model-value="image.metaData?.prompt" append-inner-icon="mdi-content-copy"
+                @click:append-inner="copy(image.metaData?.prompt)"></v-textarea>
               <v-text-field readonly density="compact" label="Model" hide-details="auto"
                 :model-value="image.metaData?.model"></v-text-field>
               <v-text-field readonly density="compact" label="Sampler" hide-details="auto"
@@ -34,7 +40,6 @@
                 :model-value="image.metaData?.cfgScale"></v-text-field>
               <v-text-field readonly density="compact" label="Steps" hide-details="auto"
                 :model-value="image.metaData?.steps"></v-text-field>
-
             </v-col>
           </v-row>
         </v-container>
@@ -45,8 +50,8 @@
   
 <script lang="ts">
 import { defineComponent } from "vue";
-import ImageDetail from "./ImageDetail.vue";
 import { ImageDatabase } from "@/types/ImageDatabase";
+import ImageDetail from "./ImageDetail.vue";
 
 export default defineComponent({
   props: ["imageId", "showImageDetailDialog"],
